f54ac52f301559ca86ed3b040ce0bd27f8869eae
syo24 <>
2023-03-13 19:05:58 +0900

    footer修正

--- components/common/c-globalFooter01/c-globalFooter01.vue
+++ components/common/c-globalFooter01/c-globalFooter01.vue
@@ -6,11 +6,10 @@
 					p.text 市来ハヤト府政対策事務所
 					p.text 住所：〒569-0804
 						br
-						| 大阪府高槻市紺屋町11-1 FKビル202号室
+						| 大阪府高槻市紺屋町11-1 FKビル2F
 					p.btn
 						a(href="https://www.google.com/maps?q=%E5%A4%A7%E9%98%AA%E5%BA%9C%E9%AB%98%E6%A7%BB%E5%B8%82%E7%B4%BA%E5%B1%8B%E7%94%BA11-1+FK%E3%83%93%E3%83%AB202%E5%8F%B7%E5%AE%A4&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjQ242Nw7z9AhXSSPUHHWk_BXkQ_AUoAXoECAEQAw", target="_blank")
 							span Google Map
-					p.text 電話番号：080-4390-0066
 				
 				.block.is-copyright
 					p.text ©Copyright 2023 Hayato Ichiki. All Rights Reserved
